c64012533fbcd3658f638d4779dfb0edb9ab28eb
王斌 <wangbin@wangbindeMacBook-Pro.local>
2024-05-12 18:21:00 +0900

    loveyou

--- 100days/js/functions.js
+++ 100days/js/functions.js
@@ -134,3 +134,35 @@ function adjustCodePosition() {
 function showLoveU() {
 	$('#loveu').fadeIn(3000);
 }
+/**
+ * 构造函数模式封装heart
+ *
+ * @param {*} body  
+ */
+function heartWith(body) {
+    this.body = body;
+    this.init = function(){
+        this.monitor();
+    };
+    this.monitor = function () {
+        var that = this;
+        document.onmousemove = function (event) {
+            var heart = document.createElement("div");
+            heart.className = "heart-with";
+            var x = event.clientX, y = event.clientY;
+            // 设置heart跟随鼠标的位置出现
+            heart.style.left = x + "px", heart.style.top = y + "px";
+            // 设置heart的大小随机 Math.random()返回一个0~1之间的数
+            var size = Math.random() * 100;
+            heart.style.width = size + "px", heart.style.height = size + "px";
+            // 将heart添加到body下
+            that.body.appendChild(heart);
+            // 设置定时器，两秒钟清理一次heart
+            setInterval(function () {
+                heart.remove();
+            }, 2000);
+        }
+    };
+
+    this.init();
+}
---
+++ 100days/js/index.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    var body = document.querySelector("body");
+    heartWith(body);
+}
+
+
